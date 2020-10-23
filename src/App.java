dd5dc05fc706e32e43847c3729cb96e64185a468
Ichsan Siswoputranto <milunes@gmail.com>
2020-10-23 21:20:29 +1100

    fix wrong logic, set array size based on the line number in file

--- src/App.java
+++ src/App.java
@@ -1,24 +1,47 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // int[] numbers = { 100, 300, 200 };
         // int[] numbers;
+        String fileToGet = "randomNumber.txt";
+        Path path = Paths.get(fileToGet);
+        long lineCount;
+        int arraySize;
 
-        try (BufferedReader in = new BufferedReader(new FileReader("randomNumber.txt"));) {
-            String strNumber;
-            int[] numbers = new int[100000];
-            int arrayIdx = 0;
-            while ((strNumber = in.readLine()) != null) {
-                numbers[arrayIdx] = Integer.parseInt(strNumber);
+        try (Stream<String> stream = Files.lines(path)) {
+            lineCount = stream.count();
+            arraySize = ((Number) lineCount).intValue();
+
+            stream.
+
+            try (BufferedReader in = new BufferedReader(new FileReader(fileToGet));) {
+                String strNumber;
+                int[] numbers = new int[arraySize];
+                int arrayIdx = 0;
+                while ((strNumber = in.readLine()) != null) {
+                    numbers[arrayIdx] = Integer.parseInt(strNumber);
+                    arrayIdx++;
+                }
+
+                System.out.println(numbers.length);
+
+                Arrays.sort(numbers);
+
+                int position = BinarySearch.rank(300, numbers);
+                // int position = Arrays.binarySearch(numbers, 300);
+
+                System.out.println(position);
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            System.out.println(numbers.length);
-
-            int position = BinarySearch.rank(300, numbers);
-
-            System.out.println(position);
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -3,13 +3,13 @@ public class BinarySearch {
     int lo = 0;
     int hi = a.length - 1;
     while (lo <= hi) {
-      System.out.println("searching ..");
 
       int mid = lo + (hi - lo) / 2;
+      System.out.println("searching: " + key + ", lo: " + lo + " hi: " + hi + ", mid: " + mid + ", a[mid]:" + a[mid]);
       if (key < a[mid])
         hi = mid - 1;
       else if (key > a[mid])
-        hi = mid + 1;
+        lo = mid + 1;
       else
         return mid;
     }
